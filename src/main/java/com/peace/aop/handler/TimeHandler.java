9fae34a64cd2a688c2f0f7298c8a62823a750b24
jayce <38997530+Jayce2018@users.noreply.github.com>
2018-07-07 16:35:40 +0800

    定时器XML实现

--- src/main/java/com/peace/aop/handler/TimeHandler.java
+++ src/main/java/com/peace/aop/handler/TimeHandler.java
@@ -10,4 +10,10 @@ public class TimeHandler {
         String date = sdf.format(new Date());
         System.out.println("==Aop:当前时间========================================:" + date);
     }
+
+    public void orderSimplePrintTime() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:ss.mm-a");
+        String date = sdf.format(new Date());
+        System.out.println("==S定时器:当前时间========================================:" + date);
+    }
 }
